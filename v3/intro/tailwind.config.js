0614bea55b509850b30328f38eaf4ea81640b0ce
seasone <contact.seasone@gmail.com>
2022-02-24 18:18:55 +0100

    tailwindcss version 3

---
+++ v3/intro/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+      '10xl': ['9rem', { lineHeight: '1' }],
+    },
+    },
+  },
+  plugins: [],
+}
